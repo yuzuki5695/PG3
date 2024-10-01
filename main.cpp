3527d5029840951041b7250601a26ed7042597f6
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-01 12:40:44 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -10,14 +10,31 @@ int Recursive(int n,int a) {
 	return (a + Recursive(n - 1,a));
 }
 
+int Recursive2(int n, int a) {
+
+	if (n == 0) {
+		return (0);
+	}
+
+	return ((a * 2 - 50) + Recursive(n - 1, a));
+}
+
+
 int main() {
 
-	int n = 100;
-	int a = 10;
+	int n = 1027;
+	int a = 3;
+	int n2 = 100;
+	int a2 = 3;
 	int result;
+	int result2;
 
 	result = Recursive(n,a);
-	printf("時給%dで%d時間働いたら%dになった\n", n, a, result);
+	result2 = Recursive2(n2, a2);
+	
+	printf("時給%d円で%d時間働いたら%d円になった\n", n, a, result);
+
+	printf("時給%d円で%d時間働いたら%d円になった\n", n2, a2, result2);
 
 	return (0);
 }
