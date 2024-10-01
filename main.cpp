1eb44d986d970581b1a699f4df7a15c37090090f
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-01 12:24:38 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include<stdio.h>
 #include<Windows.h>
 
+int Recursive(int n,int a) {
+
+	if (n == 0) {
+		return (0);
+	}
+
+	return (a + Recursive(n - 1,a));
+}
+
 int main() {
 
-	/*---文字化けを防ぐ---*/
-	//SetConsoleOutputCP(65001);
+	int n = 100;
+	int a = 10;
+	int result;
 
-	char str[] = "やきにく";
-	printf("%s", str);
+	result = Recursive(n,a);
+	printf("時給%dで%d時間働いたら%dになった\n", n, a, result);
 
-	return 0;
+	return (0);
 }
