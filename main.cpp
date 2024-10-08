81c7b73024e281148e5a59eb68ecc9033dfc0ab3
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-08 12:40:48 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -2,28 +2,10 @@
 #include<Windows.h>
 
 
-int Recursive(int n, int a) {
-
-	if (a <= 0) {
-		return (0);
-	}
-
-	return ( n +  Recursive(n * 2 - 50 ,a - 1));
-}
+int main(void) {
 
 
-int main() {
-
-	int a = 1027;
-	a *= 5;
-	int b = 100;
-	int time = 5;
-
-	int result = Recursive(b, time);
-	printf("一般的な貸金体系\n");
-	printf("時給%d円で%d時間働いた場合 = %d円になった\n", 1027, 5,a);
-	printf("再帰的な貸金体系\n");
-	printf("時給%d円で%d時間働いた場合 = %d円になった\n", b, time, result);
+	printf("main = %p\n", main);
 
 	return (0);
 }
