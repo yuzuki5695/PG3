3e19e52dbd352b15d49caf3b5daf65f5249132bd
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-09 17:09:47 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include<stdio.h>
 #include<Windows.h>
+#include<stdlib.h>
+
+typedef void (*PFunc)(int*);
+
+void DispResult(int* s) {
+	printf("%d秒待って実行されたよ\n",*s);
+}
+
+void setTimeout(PFunc p, int second) {
+	Sleep(second * 1000);
+	p(&second);
+}
 
 
 int main(void) {
 
+	int number = rand() % 2 + 0;
 
-	printf("main = %p\n", main);
+	printf("%d\n", number);
+
+	PFunc p;
+	p = DispResult;
+	setTimeout(p, 2);
+
+
 
 	return (0);
 }
