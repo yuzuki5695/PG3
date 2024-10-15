2da411ce921fa5b25ac9aeeacb2f867fba9156e3
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-15 10:31:20 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -2,29 +2,39 @@
 #include<Windows.h>
 #include<stdlib.h>
 
-typedef void (*PFunc)(int*);
-
+// コールバック関数
 void DispResult(int* s) {
-	printf("%d秒待って実行されたよ\n",*s);
+	printf("サイコロの出目は%d\n",*s);
 }
 
-void setTimeout(PFunc p, int second) {
+typedef void (*PFunc)(int*);
+
+// コールバック関数を呼び出す
+void setTimeout(PFunc p, int number, int second) {
 	Sleep(second * 1000);
-	p(&second);
+	p(&number);
 }
 
 
 int main(void) {
+	printf("サイコロを振ります\n");
+	printf("1~6の奇数か偶数を入力してください。\n");
 
-	int number = rand() % 2 + 0;
-
-	printf("%d\n", number);
-
+	int number = 0;
+	scanf_s("%dが入力されました", &number);
+	int num = rand() % 6 + 1;
+	
+	int time = 3;
+	printf("%d秒お待ちください\n",time);
+	// コールバック関数の生成
 	PFunc p;
 	p = DispResult;
-	setTimeout(p, 2);
-
-
-
+	setTimeout(p, num,time);
+	// 奇数・偶数の判定
+	if ((number % 2) == (num % 2)) {
+		printf("正解\n");
+	} else {
+		printf("不正解\n");
+	}	
 	return (0);
 }
