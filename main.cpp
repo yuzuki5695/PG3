38f3249bb5418208417b30bd93c4049c2b357752
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-30 15:48:48 +0900

    03_01

--- main.cpp
+++ main.cpp
@@ -1,40 +1,80 @@
 #include<stdio.h>
-#include<Windows.h>
 #include<stdlib.h>
-
-// コールバック関数
-void DispResult(int* s) {
-	printf("サイコロの出目は%d\n",*s);
-}
-
-typedef void (*PFunc)(int*);
-
-// コールバック関数を呼び出す
-void setTimeout(PFunc p, int number, int second) {
-	Sleep(second * 1000);
-	p(&number);
-}
-
+#include<vector>
+#include <iostream>
+#include<list>
+using namespace std;
 
 int main(void) {
-	printf("サイコロを振ります\n");
-	printf("1~6の奇数か偶数を入力してください。\n");
 
-	int number = 0;
-	scanf_s("%dが入力されました", &number);
-	int dice = rand() % 6 + 1;
-	
-	int time = 3;
-	printf("%d秒お待ちください\n",time);
-	// コールバック関数の生成
-	PFunc p;
-	p = DispResult;
-	setTimeout(p, dice,time);
-	// 奇数・偶数の判定
-	if ((number % 2) == (dice % 2)) {
-		printf("正解\n");
-	} else {
-		printf("不正解\n");
-	}	
+    // 駅名を文字列で設定
+    const char* YamanoteLine[] = {
+        "Tokyo", "Kanda", "Akihabara","Okachimachi","Ueno",
+        "Uguisudani","Nippori","Nishi-Nippori","Tabata","Komagome",
+        "Sugamo","Otska","Ikebukuro","Mejire","Takadanobaba",
+        "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+        "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+        "Takanawa Gateway","Tamachi","Hamamatscho","Shimbashi","Yurakucho"
+    };
+
+    // 駅名リスト
+    list<const char*> list1970(YamanoteLine, YamanoteLine + 30);
+    list<const char*> list2019(YamanoteLine, YamanoteLine + 30);
+    list<const char*> list2022(YamanoteLine, YamanoteLine + 30); 
+    // 駅の数をカウント
+    int count = 0;
+
+    printf("山手線の路線図\n\n");
+
+    // 双方向リストのイテレータを使い出力 
+    printf("1970年\n");
+    for (auto itr = list1970.begin(); itr != list1970.end(); ++itr) {
+        printf("%s", *itr);
+        count++;
+        // 改行
+        if (count % 10 == 0 && count <= 25) {
+            printf("\n");
+            printf("⇒ ");
+        } else if (count < 30) {
+            printf(" ⇒ ");
+        }
+    }
+    printf("\n\n\n");
+    count = 0;
+
+    printf("2019年\n");
+    for (auto itr = list2019.begin(); itr != list2019.end(); ++itr) {
+        // 駅名を表示
+        printf("%s", *itr);
+        count++;
+
+        if (count == 25) {
+            itr = list2019.erase(itr); // 高輪ゲートウェイ削除
+        } else  {
+            // 改行
+            if (count % 10 == 0 && count <= 25) {
+                printf("\n");
+                printf("⇒ ");
+            } else if (count < 30) {
+                printf(" ⇒ ");
+            }
+        }
+    }
+    printf("\n\n\n");
+    count = 0;
+
+    printf("2022年\n");
+    for (auto itr = list2022.begin(); itr != list2022.end(); ++itr) {
+        printf("%s", *itr);
+        count++;
+        // 改行
+        if (count % 10 == 0 && count <= 25) {
+            printf("\n");
+            printf("⇒ ");
+        } else if(count < 30) {
+            printf(" ⇒ ");
+        }
+    }
+    printf("\n\n\n");
 	return (0);
 }
