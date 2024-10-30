e619350e1fe65e48eb1703d1bab0c0b10ea4f88b
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-30 16:04:29 +0900

    03_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<vector>
-#include <iostream>
 #include<list>
 using namespace std;
 
@@ -22,25 +21,38 @@ int main(void) {
     list<const char*> list2019(YamanoteLine, YamanoteLine + 30);
     list<const char*> list2022(YamanoteLine, YamanoteLine + 30); 
     // 駅の数をカウント
+    int Maxcount = 30;
     int count = 0;
 
     printf("山手線の路線図\n\n");
-
-    // 双方向リストのイテレータを使い出力 
+    /*------------双方向リストのイテレータを使い出力------------*/
     printf("1970年\n");
     for (auto itr = list1970.begin(); itr != list1970.end(); ++itr) {
+        // 駅名を表示
         printf("%s", *itr);
         count++;
-        // 改行
-        if (count % 10 == 0 && count <= 25) {
-            printf("\n");
-            printf("⇒ ");
-        } else if (count < 30) {
+
+        if (count == 7) {
+            itr = list1970.erase(itr); // 西日暮里削除
             printf(" ⇒ ");
+            Maxcount--;
+        }else if (count == 25) {
+            itr = list1970.erase(itr); // 高輪ゲートウェイ削除
+            printf(" ⇒ ");
+            Maxcount--;
+        }else {
+            // 改行
+            if (count % 10 == 0 && count <= 25) {
+                printf("\n");
+                printf("⇒ ");
+            } else if (count < Maxcount) {
+                printf(" ⇒ ");
+            }
         }
     }
     printf("\n\n\n");
     count = 0;
+    Maxcount = 30;
 
     printf("2019年\n");
     for (auto itr = list2019.begin(); itr != list2019.end(); ++itr) {
@@ -50,18 +62,21 @@ int main(void) {
 
         if (count == 25) {
             itr = list2019.erase(itr); // 高輪ゲートウェイ削除
+            printf(" ⇒ ");
+            Maxcount--;
         } else  {
             // 改行
             if (count % 10 == 0 && count <= 25) {
                 printf("\n");
                 printf("⇒ ");
-            } else if (count < 30) {
+            } else if (count < Maxcount) {
                 printf(" ⇒ ");
             }
         }
     }
     printf("\n\n\n");
     count = 0;
+    Maxcount = 30;
 
     printf("2022年\n");
     for (auto itr = list2022.begin(); itr != list2022.end(); ++itr) {
@@ -71,7 +86,7 @@ int main(void) {
         if (count % 10 == 0 && count <= 25) {
             printf("\n");
             printf("⇒ ");
-        } else if(count < 30) {
+        } else if(count < Maxcount) {
             printf(" ⇒ ");
         }
     }
