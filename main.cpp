e1f0468bd7045d6d061075f4f0d4a05079dad1ff
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-11-26 10:54:57 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include<stdio.h>
-#include <iostream>
-#include <thread>
-
-static void printMessage(const std::string& message) {
-    std::cout << message << std::endl;
-}
 
 int main() {
-    std::thread t1(printMessage, "thread1");
-    t1.join();
-    std::thread t2(printMessage, "thread2");
-    t2.join();
-    std::thread t3(printMessage, "thread3");
-    t3.join();
 
     return 0;
 }
