9f88faba38026642dfc17797613092be944fd1f1
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-01-28 10:11:06 +0900

    07_01_完了

--- main.cpp
+++ main.cpp
@@ -1,33 +1,31 @@
-#include<stdio.h>
-#include<chrono>
-#include<string>
+#include <stdio.h>
+#include <chrono>
+#include <string>
 #include <iostream>
 
-void measureCopyAndMove() {
-    // 100万文字の'a'で初期化
-    std::string a(100000, 'a');
-    std::string b; // コピー先
-    std::string c; // ムーブ先
+int main() {
 
+    printf_s("100,000文字を移動とコピーで比較しました。\n");
+    // 10万文字の'a'で初期化
+    std::string original(100000, 'a');
+    std::string copyTarget;
+    std::string moveTarget;
+
+    // コピー計測
     auto startCopy = std::chrono::high_resolution_clock::now();
-    b = a;
+    copyTarget = original;
     auto endCopy = std::chrono::high_resolution_clock::now();
+    // 移動計測
     auto startMove = std::chrono::high_resolution_clock::now();
-    c = std::move(a);
+    moveTarget = std::move(original);
     auto endMove = std::chrono::high_resolution_clock::now();
 
+    // コピーと移動の時間を計測
     auto copyDuration = std::chrono::duration_cast<std::chrono::microseconds>(endCopy - startCopy).count();
     auto moveDuration = std::chrono::duration_cast<std::chrono::microseconds>(endMove - startMove).count();
 
-    printf_s("コピー : %lld us\n", copyDuration);
-    printf_s("移動 : %lld us\n", moveDuration);
-    //// doubleに変換して表示
-    //printf_s("Copy duration: %.3f us\n", static_cast<double>(copyDuration));
-    //printf_s("Move duration: %.3f us\n", static_cast<double>(moveDuration));
-}
+    printf_s("コピー　　: %.5f us\n", static_cast<double>(copyDuration));
+    printf_s("移動　　　: %.5f us\n", static_cast<double>(moveDuration));
 
-int main() {
-    printf_s("100,000文字を移動とコピーで比較しました。\n");
-    measureCopyAndMove();
     return 0;
 }
